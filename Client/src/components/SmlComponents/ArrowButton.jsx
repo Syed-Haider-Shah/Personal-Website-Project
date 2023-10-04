1748131eb6bb426582453b462d0a68f299bdfb35
Syed-Haider-Shah <haider123123123@gmail.com>
2023-10-05 03:30:47 +1100

    Small components adjusted

--- Client/src/components/SmlComponents/ArrowButton.jsx
+++ Client/src/components/SmlComponents/ArrowButton.jsx
@@ -3,11 +3,11 @@ export default function ArrowButton({ name, symbol, route }) {
   return (
     <Link
       to={route}
-      className="px-1 transition-all flex justify-between  hover:bg-indigo-700 hover:text-white"
+      className="px-1 transition-all flex justify-between text-xl hover:bg-indigo-500 hover:text-white"
     >
       <div className="w-fit">{name}</div>
       <div className="w-fit">
-        <i className={symbol}></i>
+        <i className="fa-solid fa-arrow-right"></i>
       </div>
     </Link>
   );
--- Client/src/components/SmlComponents/Card2.jsx
+++ Client/src/components/SmlComponents/Card2.jsx
@@ -1,5 +1,5 @@
-import Checkbox from "./Checkbox";
-export default function Card1({ heading, checklist }) {
+import ArrowButton from "./ArrowButton";
+export default function Card2({ heading, checklist }) {
   return (
     <div className=" w-[30%] shadow-lg rounded bg-white pt-5">
       <h1 className="m-2 mt-5 text-3xl font-bold text-indigo-500 bg-red- text-center">
@@ -7,7 +7,7 @@ export default function Card1({ heading, checklist }) {
       </h1>
       <div className="mt-10 px-12 ">
         {checklist.map((check) => (
-          <Checkbox key={name} name={check} />
+          <ArrowButton key={name} name={check} />
         ))}
       </div>
     </div>
