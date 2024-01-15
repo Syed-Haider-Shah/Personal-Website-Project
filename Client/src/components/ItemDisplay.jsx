779ff9f691f358f6b99eeed21be9d8f5c1ff14c9
Syed-Haider-Shah <140457553+Syed-Haider-Shah@users.noreply.github.com>
2024-01-16 00:18:05 +1100

    Add to cart button added and a bit of styling added

--- Client/src/components/ItemDisplay.jsx
+++ Client/src/components/ItemDisplay.jsx
@@ -3,6 +3,10 @@ import Product from "./SmlComponents/Product";
 const ItemDisplay = () => {
   return (
     <>
+      <div>
+        <h>Items for Sale</h>
+        <p>Add them to cart and then view the cart</p>
+      </div>
       <div className="flex gap-10 m-40 place-items-center justify-center">
         {PRODUCTS.map((product) => (
           <Product key={product.id} data={product} />
--- Client/src/components/SmlComponents/Product.jsx
+++ Client/src/components/SmlComponents/Product.jsx
@@ -4,9 +4,12 @@ const Product = (props) => {
     <>
       <div className="">
         <img src={productImage} />
-        <div className="flex  flex-col items-center justify-center">
-          <p className="font-bold">{productName}</p>
-          <p>${price}</p>
+        <div className="flex gap-1 flex-col items-center justify-center">
+          <p className="font-bold text-lg">{productName}</p>
+          <p className="text-md">${price}</p>
+          <button className="font-bold hover:text-slate-700 text-sm hover:bg-white border-2 border-slate-700 bg-slate-700 text-white p-1 px-3 transition-all shadow-lg rounded-full">
+            Add to Cart
+          </button>
         </div>
       </div>
     </>
