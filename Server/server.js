65aae990e395274de52ca5faa6b22d4073efbfff
Syed-Haider-Shah <140457553+Syed-Haider-Shah@users.noreply.github.com>
2024-01-21 19:57:47 +1100

    all allowed

--- Server/server.js
+++ Server/server.js
@@ -6,13 +6,7 @@ const PORT = process.env.PORT || 8000; //port for deployment, otherwise use 8000
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ["https://reno-pilots-front-end.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.get("/", cors(), (req, res) => {
   res.json("Hello");
