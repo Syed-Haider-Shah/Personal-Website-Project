c8a672fcd5e971c3f965119dc77278e2773a7610
Syed-Haider-Shah <140457553+Syed-Haider-Shah@users.noreply.github.com>
2024-01-21 19:13:55 +1100

    vercel

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,13 @@ const PORT = process.env.PORT || 8000; //port for deployment, otherwise use 8000
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.get("/", cors(), (req, res) => {
   res.json("Hello");
