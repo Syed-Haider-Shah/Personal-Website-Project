233d6b6f9e2f97008cf5c86413241e049e7520e4
Syed-Haider-Shah <140457553+Syed-Haider-Shah@users.noreply.github.com>
2024-06-10 16:59:09 +1000

    ShopContext moved to outer file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+import { ShopContextProvider } from './components/SmlComponents/ShopContext.jsx';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <ShopContextProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ShopContextProvider>
 );
