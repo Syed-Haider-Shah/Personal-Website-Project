d495771a35f280cb8823674daea099647ea97278
Syed-Haider-Shah <140457553+Syed-Haider-Shah@users.noreply.github.com>
2024-06-10 18:15:15 +1000

    useroute hook is added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { ShopContextProvider } from './components/SmlComponents/ShopContext.jsx';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ShopContextProvider>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </ShopContextProvider>
 );
